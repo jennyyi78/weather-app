912f170183546a867446dd04afb032b68590f6c4
Jenny <jennyyi78@gmail.com>
2021-01-23 00:51:18 -0500

    Update fetch url

--- src/modules/getWeather.js
+++ src/modules/getWeather.js
@@ -1,5 +1,5 @@
 const getWeather = async (location) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=4eab9c5e984dcd0a2d0ceaad08901dda`, { mode: 'cors' });
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=4eab9c5e984dcd0a2d0ceaad08901dda`, { mode: 'cors' });
   const weatherData = await response.json();
   return weatherData;
 };
